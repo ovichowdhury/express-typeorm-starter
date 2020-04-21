6e99cd062a7ccfb0df0b4e0c3c527284738be5b9
ovichowdhury <ovi.chowdhury@hotmail.com>
2020-04-21 19:59:36 +0600

    account delete added

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -63,19 +63,19 @@ router.post('/applicant/picture', wrap(async (req: Request, res: Response, next:
     applicantPic.data = Buffer.from(req.body.data, 'base64');
     applicantPic.mimeType = req.body.mimeType;
     await getManager().getRepository(ApplicantPicture).save(applicantPic);
-    const results = await getManager().getRepository(Applicant).update(req.body.applicantId, {applicantPicture: applicantPic});
+    const results = await getManager().getRepository(Applicant).update(req.body.applicantId, { applicantPicture: applicantPic });
     return res.status(201).json(results);
 }));
 
 // update account
 router.put('/', wrap(async (req: Request, res: Response, next: NextFunction) => {
-    const results = await getManager().getRepository(Account).update(req.body.accountId, {name: req.body.name});
+    const results = await getManager().getRepository(Account).update(req.body.accountId, { name: req.body.name });
     return res.status(200).json(results);
 }));
 
 // update applicant
 router.put('/applicant', wrap(async (req: Request, res: Response, next: NextFunction) => {
-    const results = await getManager().getRepository(Applicant).update(req.body.applicantId, {name: req.body.name, nid: req.body.nid});
+    const results = await getManager().getRepository(Applicant).update(req.body.applicantId, { name: req.body.name, nid: req.body.nid });
     return res.status(200).json(results);
 }));
 
@@ -88,8 +88,34 @@ router.get('/', wrap(async (req: Request, res: Response, next: NextFunction) => 
 
 // get applicant account from applicant id
 router.get('/applicant/:applicantId', wrap(async (req: Request, res: Response, next: NextFunction) => {
-    const results = await getManager().findOne(Applicant, parseInt(req.params.applicantId), {relations: ["account", "applicantPicture"]});
+    const results = await getManager().findOne(Applicant, parseInt(req.params.applicantId), { relations: ["account", "applicantPicture"] });
     return res.status(200).json(results);
+}));
+
+
+// delete account with applicant and applicant picture
+router.delete('/:accountId', wrap(async (req: Request, res: Response, next: NextFunction) => {
+    await getManager().transaction(async tem => {
+        const accountApplicant: any = await tem.getRepository(Account).createQueryBuilder("account")
+                                        .select("account.id")
+                                        .addSelect("applicant.id")
+                                        .leftJoin("account.applicants", "applicant")
+                                        .where("account.id = :id", { id: req.params.accountId })
+                                        .getOne();
+        for (const applicant of accountApplicant.applicants) {
+            const applicantPictureId = await tem.getRepository(Applicant).createQueryBuilder("applicant")
+                                            .select("applicant.id")
+                                            .addSelect("applicantPicture.id")
+                                            .innerJoin("applicant.applicantPicture", "applicantPicture")
+                                            .where("applicant.id = :id", { id: applicant.id })
+                                            .getOne();
+            if(applicantPictureId !== undefined)
+                await getManager().getRepository(ApplicantPicture).delete(applicantPictureId.id);
+            await getManager().getRepository(Applicant).delete(applicant.id);
+        }
+        const results = await getManager().getRepository(Account).delete(req.params.accountId);
+        return res.status(200).json(results);
+    });
 }));
 
 
